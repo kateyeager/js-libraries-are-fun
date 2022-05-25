0dfc10eb72670c4c0f2b3cc1af420a63b2510ada
kyeager <kyeager@ewu.edu>
2022-05-25 12:14:02 -0700

    Moving from CodePen to GitHub

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # my-boilerplate
+
+# CircleType.js v1
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/kyeager/pen/oNEwjNj](https://codepen.io/kyeager/pen/oNEwjNj).
--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 
 console.log('Page Loaded 👍')
+
+// <h2 id="demo3">This text makes a complete rotation no matter how long it is. </h2>
+new CircleType(document.getElementById("demo3"));
+
